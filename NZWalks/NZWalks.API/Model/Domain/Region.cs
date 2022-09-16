5f7cb1bfd92a877760d5674178912b7b2d277307
Jansyp <janezhil10@gmail.com>
2022-09-16 11:00:45 +0530

    End of Session 2

---
+++ NZWalks/NZWalks.API/Model/Domain/Region.cs
@@ -0,0 +1,16 @@
+﻿namespace NZWalks.API.Model.Domain
+{
+    public class Region
+    {
+        public Guid Id{ get; set; }
+        public string Code { get; set; }
+        public string Name{ get; set; }
+        public double area{ get; set; }
+        public double Lat { get; set; }
+        public double Long { get; set; }
+        public long Population { get; set; }
+
+
+
+    }
+}
